f8d7059058f63cc14dcb80e28b33ad4cedc41da7
tlambert42 <100758874+tlambert42@users.noreply.github.com>
2025-07-15 13:06:09 +0200

    Add files via upload

---
+++ pharmacy_service_jira_css.js
@@ -0,0 +1,129 @@
+// ==UserScript==
+// @name        New script atlassian.net
+// @namespace   Violentmonkey Scripts
+// @match       https://galenica.atlassian.net/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 14.07.2025 11:05:34
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+
+div._1r04ze3t._kqswstnw {
+  display: none;
+}
+div._16jlkb7n._1o9zkb7n._i0dlf1ug._1reo1wug._18m91wug {
+  background-color: #E9F2FE;
+  border-right:1px solid #1868DB;
+}
+
+div._ca0qutpp._u5f3utpp._n3tdutpp._19bvutpp._7myae4h9._1sw7nqa1._qgnumuej._bfhkhp5a {
+  display: none;
+}
+
+header._nd5l8cbt._zulpu2gc._18zrutpp._179rzgxb._1e0c11p5._yv0e1mfv._4cvr1h6o._bfhkvuon._vchhusvi._4t3i1dgc._152t1nws._kqsw1if8._1pbyegat._d6vu1bgi._1j8b18ax {
+  border-bottom:1px solid #1868DB;
+  background-color: #69A5EB;
+
+}
+div._ca0qutpp._u5f3utpp._n3tdutpp._19bvutpp._7myae4h9._1sw7nqa1._qgnumuej {
+  display:none;
+}
+/* FILTRES */
+
+.css-wo70zv{
+  display:none;
+}
+.css-1y9f6yn{
+  display:none;
+}
+.css-1qn711l{
+  display:none;
+}
+._1xoqidpf:last-child{
+  display:none;
+}
+.css-1sluwpb{
+  display:none;
+}
+/*------------------*/
+._165nglyw{
+  display:none;
+}
+._bozg1crf._1e0cglyw{
+  display:none;
+}
+/* Menu droite */
+/* A FAIRE */
+._4t3i1ris{
+  background-color:RED;
+}
+._2hwxyrq6{
+  background-color:#E9F2FE;
+  border-left:1px solid #1868DB;
+  margin-left:10px;
+}
+._otyru2gc{
+  background-color: #fff;
+}
+.css-38ytem{
+  background-color: #69A5EB !important;
+}
+.css-38ytem h2{
+  color:#fff;
+}
+._16qs1pcf{
+  background-color: #1868DB;
+}
+._16qs1pcf button .css-178ag6o{
+  color: #fff;
+}
+._16qs1pcf .css-5a6fwh{
+  color: #fff;
+}
+.eDdpIk{
+  background-color: #1868DB;
+}
+.eDdpIk ._1wybidpf{
+  color: #fff;
+  }
+#jira-issue-header-actions{
+  background-color: #E9F2FE;
+}
+/* Timesheet */
+
+
+
+.css-1qp4wby{
+  display: none;
+}
+
+div._19pkidpf._2hwx1wug._otyridpf._18u01wug._4cvr1h6o._1bahesu3._1e0c1txw._1bsb1osq._vchhusvi._10nju2gc._p12f1ayu._7zw61ejb {
+  display: none;
+}
+
+._ca0qxy5q{
+  padding-top: 0px !important;
+}
+/*
+._11lvidpf button {
+  display:none;
+}
+
+._1e0c1txw._4cvr1h6o._1bah1yb4._1tkezwfg.css-1bgzpx9{
+  display:none;
+}
+*/
+/* INFO PHARMA */
+._16qszucj{
+  background-color:#d5efe2;
+  box-shadow:0px 1px 3px #076837;
+
+}
+
+
+
+`);
+
